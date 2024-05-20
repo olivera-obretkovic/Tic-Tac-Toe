a64b8182d1dfe32f1f50f16d88ae478711ef2f88
olivera-obretkovic <oliveraobretkovic.inf@gmail.com>
2024-05-20 17:41:15 +0200

    Creating the game logic

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+import "./index.css";
+import Board from "./components/Board";
+
+export default function App() {
+  return <Board />;
+}
---
+++ src/components/Square.tsx
@@ -0,0 +1,12 @@
+import "../App.css";
+import "../index.css";
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+export default Square;
